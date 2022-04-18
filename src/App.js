c202f15dc1b58d2417cf6a218c0d717d14ce8211
AriestaPurwa <ariestapurwanugraha55@gmail.com>
2022-04-18 13:23:39 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import react from 'react';
+import utama from'./components/utama';
+import {link} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class app extends react.component {
+  render() {
+    return(
+      <div> <hr>
+      <link to="\">beranda</link> |
+      <link to="\tentangsaya">tentang saya</link> |
+      <link to="\karya">karya</link> |
+      <link to="\kontak">kontak</link></hr> 
+      <p>utama /</p>
+      </div>
+    );
+  }
 }
 
 export default App;
